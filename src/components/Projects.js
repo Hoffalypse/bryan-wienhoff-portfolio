2cf4411630fd1d2841e7183d90fea60214adbc7e
Bryan Wienhoff <bhoff1980@gmail.com>
2022-12-29 23:39:53 -0700

    adding data to portfolio objects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,31 +7,32 @@ function Projects() {
 
   let works = [
     {
-      img: require('./img-files/bando.jpg'),
-      alt:"Band Search App",
-      link: "https://joseeenrique.github.io/Music-App/",
-      name: 'Band Search App',
-      gitlink: 'https://github.com/joseeenrique/Music-App',
-      desc: 'This is the bando ba desc',
-      tool: 'tip1'
-
-    },
-    {
       img: require('./img-files/mythic.jpg'),
       alt:"WOW Team Builder",
       link: "https://mythic-plus-team.herokuapp.com/",
       name: 'WOW Team Builder',
       gitlink: ' https://github.com/Hoffalypse/mythic-plus-team',
-      desc: 'This is WOW Team Builder desc',
+      desc: 'This App creates teams for World of Warcraft Mythic+ battles. <br /> Full Stack application using handlebars on the frontend<br /> Node, Express, MySQL on Backend utilizing multiple APIs', 
       tool: 'tip2'
     },
+    {
+      img: require('./img-files/bando.jpg'),
+      alt:"Band Search App",
+      link: "https://joseeenrique.github.io/Music-App/",
+      name: 'Band Search App',
+      gitlink: 'https://github.com/joseeenrique/Music-App',
+      desc: 'This App is a one stop shop for all band information.<br /> Frontend application using Javascript, CSS and HTML. <br />Recieves API data from Ticketmsater and theAudioDB ',
+      tool: 'tip1'
+
+    },
+   
     {
       img: require('./img-files/weather.jpg'),
       alt:"Weather App",
       link: "https://hoffalypse.github.io/Current-Weather/",
       name: 'Current Weather',
       gitlink: 'https://github.com/Hoffalypse/Current-Weather',
-      desc: 'This is the fire fighter desc',
+      desc: 'This add gives Current Weather Data World Wide!<br />Uses Javascript, CSS and HTML for a clean UI/UX<br/> API data from OpenWeatherMap also utilizes local storage',
       tool: 'tip3'
     },
     {
@@ -40,7 +41,7 @@ function Projects() {
       link: "https://stormy-mesa-72944.herokuapp.com/",
       name: 'The Tech Blog',
       gitlink: 'https://github.com/Hoffalypse/Tech-Blog',
-      desc: 'loooooooooooooooooooooooooooooooooooong',
+      desc: 'Lost in a field of bugs? The Tech blog has Answers<br/> Full stack application using Javascript on the Frontend <br/>Node, Express and MySQL on the backend <br/>Clean Interface with encrypted logins   ',
       tool: 'tip4'
     },
     {
@@ -73,6 +74,7 @@ function Projects() {
     <section className="pad-img">
       <div className="row">
         <p className="head">My Work</p>
+        <p className='error-text'>Hover on name for Description</p>
         { works.map((result) => (
         <div className="col-xxl-4 col-lg-6">
         
@@ -87,7 +89,7 @@ function Projects() {
           </div>
           <a className='text-on-image' id={result.tool}> {result.name} </a>
 
-        <Tooltip anchorId={result.tool} content={result.desc} />
+        <Tooltip anchorId={result.tool} html={result.desc} />
      
           
           <a 
