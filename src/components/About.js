73bf4edd919992df4758412e27bdf92ea55354f6
Bryan W <bhoff1980@gmail.com>
2023-03-06 03:41:48 -0700

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,8 @@ function About() {
               
                   <p className="about-me"><span>I</span> <span>was</span> <span>born</span> <span>and</span> <span>raised</span> <span>in</span> <span>Illinois</span> <span>where</span> <span>I</span> <span>completed</span> <span>my</span> <span>degree</span> <span>in</span> <span>International</span> <span>Relations</span> <span>at</span> <span>SIU</span> <span>Carbondale</span> <span>(Go Salukis!). &nbsp;</span> 
                   <span>I</span> <span>enjoy</span> <span>all</span> <span>things</span> <span>outdoors:</span> <span>hiking,</span> <span>climbing,</span> <span>rafting,</span> <span>fishing</span> <span>and</span> <span>off-roading. &nbsp;</span>
-                  <span>My</span> <span>background</span> <span>is</span> <span>in</span> <span>the</span> <span>Financial</span> <span>Services</span> <span>and</span> <span>Casino</span> <span>Entertainment</span> <span>Industries.</span> <span>I</span> <span>also</span> <span>spent</span> <span>many</span> <span>years</span> <span>as</span> <span>a</span> <span>professional</span> <span>poker</span> <span>player. &nbsp;</span><span>I</span> <span>have</span> <span>recently</span> <span>started a &nbsp;</span> 
-                  <span>new</span> <span>career</span> <span>in</span> <span>Web</span> <span>Development.</span></p>
+                  <span>My</span> <span>background</span> <span>is</span> <span>in</span> <span>the</span> <span>Financial</span> <span>Services</span> <span>and</span> <span>Casino</span> <span>Entertainment</span> <span>Industries.</span> <span>I</span> <span>also</span> <span>spent</span> <span>many</span> <span>years</span> <span>as</span> <span>a</span> <span>professional</span> <span>poker</span> <span>player. &nbsp;</span><span>Software</span> <span>development</span> <span>has</span> <span>been &nbsp;</span> 
+                  <span>my</span> <span>passion</span> <span>for</span> <span>many</span> <span>years.</span></p>
                   <div className="centerpic" >
                   <img 
                   className="mtn hoff" 
